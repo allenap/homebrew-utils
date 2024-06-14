e53ccec912fb12b3aa941663a059cf3fe53b3260
Gavin Panella <gavin@allenap.me>
2024-06-14 12:13:31 +0000

    unison-confgen: add 0.2.0 bottle.

--- Formula/unison-confgen.rb
+++ Formula/unison-confgen.rb
@@ -6,6 +6,13 @@ class UnisonConfgen < Formula
   license "GPL-3.0-or-later"
 
   head "https://github.com/allenap/unison-confgen.git", branch: "master"
+
+  bottle do
+    root_url "https://ghcr.io/v2/allenap/utils"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura:      "4fbd8344fe47816ad9eb741ad7531405d92fc44dde2b1bbbd4e7e08d60176685"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "4bb89a77a3e40e687e60dad22bd7ba6c8d6adce88c0f83258b9954260fd576db"
+  end
   depends_on "rust" => :build
 
   def install
