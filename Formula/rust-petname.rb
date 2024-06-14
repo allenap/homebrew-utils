190cbe5633ae9f20f6891f8448a1cf04025dc8b5
Gavin Panella <gavin@allenap.me>
2024-06-14 12:13:31 +0000

    rust-petname: add 2.0.2 bottle.

--- Formula/rust-petname.rb
+++ Formula/rust-petname.rb
@@ -6,6 +6,13 @@ class RustPetname < Formula
   license "Apache-2.0"
 
   head "https://github.com/allenap/rust-petname.git", branch: "master"
+
+  bottle do
+    root_url "https://ghcr.io/v2/allenap/utils"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, ventura:      "158e882edc8172b4a3839e8b7ec9638a194b84615e9088aa03129d5ea6a3a0cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "df5c135ee5b460f5e7a02d8ac7078c997308d0be5bf55619141a2660e1d8a1d9"
+  end
   depends_on "rust" => :build
 
   def install
