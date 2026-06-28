b2148c1a39185ddb6b453d3bbd0348799e7116b7
Gavin Panella <gavin@allenap.me>
2026-06-28 07:37:10 +0000

    rust-petname: add 3.0.1 bottle.

--- Formula/rust-petname.rb
+++ Formula/rust-petname.rb
@@ -12,6 +12,13 @@ class RustPetname < Formula
     regex(/^v(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/allenap/utils"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "ef30489fef6adabf48c4fc2eb56fb259c8419078f07e2ad977e246cbbfca93ca"
+    sha256 cellar: :any,                 x86_64_linux: "637f7d79647ae5d918e9510bce9d0891506c2d7da5272973ee934c277bf8e20c"
+  end
+
   depends_on "rust" => :build
 
   def install
