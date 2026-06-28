b8d8da3a17bc4364161dc590ac80965a6ccca23c
Gavin Panella <gavinpanella@gmail.com>
2026-06-28 09:30:59 +0200

    rust-petname: drop stale 3.0.0 bottle block
    The 3.0.1 bump kept the 3.0.0 bottle stanzas, so brew installs 404 on a
    3.0.1 bottle that was never published. Remove the stale block; pr-pull will
    re-add the correct 3.0.1 stanzas from freshly-built bottles.
    
    Closes #12.
    
    Signed-off-by: Gavin Panella <gavin@allenap.me>

--- Formula/rust-petname.rb
+++ Formula/rust-petname.rb
@@ -12,11 +12,6 @@ class RustPetname < Formula
     regex(/^v(\d+(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    root_url "https://ghcr.io/v2/allenap/utils"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "7fed414ba5309177b61eea996b1ffbf5b484aa100f724ba2d24a24c946809363"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "98baa12f59094cba836ecb8cf8548ea2b3f1a269adb8d339d550604ab6f5c4be"
-  end
   depends_on "rust" => :build
 
   def install
